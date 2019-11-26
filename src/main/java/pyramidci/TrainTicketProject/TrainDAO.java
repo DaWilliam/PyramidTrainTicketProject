5fec8f675ed2ce769630e00778f6e79d7976ec6b
TheWilliam <will3shu@gmail.com>
2019-11-26 16:54:46 -0500

    Removed test files. Added Created Ticket and TrainDAO files

---
+++ src/main/java/pyramidci/TrainTicketProject/TrainDAO.java
@@ -0,0 +1,13 @@
+package pyramidci.TrainTicketProject;
+
+public class TrainDAO {
+	private String DRIVER_NAME;
+	private String DB_URL;
+	private String USERNAME;
+	private String PASSWORD;
+	
+	public Train findTrain(int trainNo)
+	{
+		
+	}
+}
