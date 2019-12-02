6691964d79f1af25b9166a250135f1ed9bf42743
Bao Nguyen <baovu88@yahoo.com>
2019-12-02 16:14:21 -0500

    implimentation of TrainDAO and DBconnection

--- src/main/java/pyramidci/TrainTicketProject/TrainDAO.java
+++ src/main/java/pyramidci/TrainTicketProject/TrainDAO.java
@@ -1,14 +1,31 @@
 package pyramidci.TrainTicketProject;
 
+import java.sql.*;
+
 public class TrainDAO {
 	//Variables
 	private String DRIVER_NAME;
-	private String DB_URL;
-	private String USERNAME;
-	private String PASSWORD;
-	
+	private String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	private String USERNAME = "hr";
+	private String PASSWORD = "hr";
+	DBConnection dbc = new DBConnection();
+	Connection con = dbc.getConnection(DB_URL, USERNAME, PASSWORD);
 	public Train findTrain(int trainNo)
 	{
-		
+		PreparedStatement stmt;
+		ResultSet rs;
+		String query;
+		try {
+			query = "select * from trains where train_no = ?";
+			stmt = con.prepareStatement(query);
+			stmt.setInt(1,trainNo);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				return new Train(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
